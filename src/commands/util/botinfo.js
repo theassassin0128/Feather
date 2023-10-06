2826523ba092959bfab63095397bb446234498f5
theassassin0128 <rafisarkar46@gmail.com>
2023-10-07 00:00:12 +0600

    botinfo.js

---
+++ src/commands/util/botinfo.js
@@ -0,0 +1,82 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  version,
+  ChatInputCommandInteraction,
+} = require("discord.js");
+const { colours } = require("../../config.json");
+const pkg = require("../../../package.json");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("botinfo")
+    .setDescription("Replies with current stats of the bot."),
+  test: true,
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
+   * @returns
+   */
+  execute: async (interaction, client) => {
+    let days = Math.floor(client.uptime / 86400000);
+    let hours = Math.floor(client.uptime / 3600000) % 24;
+    let minutes = Math.floor(client.uptime / 60000) % 60;
+    let seconds = Math.floor(client.uptime / 1000) % 60;
+
+    let ping = client.ws.ping;
+
+    let colour = {
+      green: "🟢",
+      yellow: "🟡",
+      red: "🔴",
+    };
+
+    const stats = new EmbedBuilder()
+      .setColor(colours.main)
+      .setTitle("__GENERAL INFO__")
+      .setDescription(
+        [
+          `**Name :** ${client.user.username}`,
+          `**Tag :** ${client.user.tag}`,
+          `**Version :** ${pkg.version}`,
+          `**Website :** Coming soon.`,
+        ].join("\n")
+      )
+      .setThumbnail(client.user.avatarURL({ size: 4096 }))
+      .addFields(
+        {
+          name: "__BOT STATUS__",
+          value: [
+            `**Status** :  \`🟢\` Online`,
+            `**Ping** : \`${
+              ping <= 200
+                ? colour.green
+                : ping <= 400
+                ? colour.yellow
+                : colour.red
+            }\` ${ping}ms`,
+            `**Uptime** :\n\`\`\`\n${days}Days, ${hours}Hours, ${minutes}Minutes, ${seconds}Seconds\n\`\`\``,
+          ].join("\n"),
+        },
+        {
+          name: "__LANGUAGE & LIBRARY INFO__",
+          value: [
+            `**Name :** [nodejs](https://nodejs.org/en/) | ${process.version}`,
+            `**Library :** [discord.js](https://discord.js.org/#/) | ${version}`,
+          ].join("\n"),
+        },
+        {
+          name: "__GITHUB REPOSITORY__",
+          value: [
+            `**Name :** [Feather](https://github.com/theassassin0128/Feather#README.md)`,
+            `**Owner :** [theassassin0128](https://github.com/theassassin0128)`,
+          ].join("\n"),
+        }
+      );
+
+    interaction.reply({
+      embeds: [stats],
+    });
+  },
+};
--- src/commands/util/invite.js
+++ src/commands/util/invite.js
@@ -1,43 +1,42 @@
 const {
-	ButtonBuilder,
-	ActionRowBuilder,
-	ButtonStyle,
-	resolvePartialEmoji,
-	Client,
-	ChatInputCommandInteraction,
-	OAuth2Scopes,
+  ButtonBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+  resolvePartialEmoji,
+  Client,
+  ChatInputCommandInteraction,
+  OAuth2Scopes,
+  SlashCommandBuilder,
 } = require("discord.js");
 
 module.exports = {
-	data: {
-		name: "invite",
-		type: 1,
-		description: "Replies with a list of my invite links.",
-		dm_permission: false,
-	},
-	test: true,
-	/**
-	 *
-	 * @param {ChatInputCommandInteraction} interaction
-	 * @param {Client} client
-	 */
-	execute: async (interaction, client) => {
-		let link = client.generateInvite({
-			permissions: BigInt(1539745246838),
-			scopes: [OAuth2Scopes.Bot, OAuth2Scopes.ApplicationsCommands],
-		});
-		const button = new ButtonBuilder()
-			.setLabel("Invite Link")
-			.setStyle(ButtonStyle.Link)
-			.setURL(link)
-			.setEmoji(resolvePartialEmoji("✉️"));
+  data: new SlashCommandBuilder()
+    .setName("invite")
+    .setDescription("Replies with a list of my invite links.")
+    .setDMPermission(false),
+  test: true,
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
+   */
+  execute: async (interaction, client) => {
+    let link = client.generateInvite({
+      permissions: BigInt(1539745246838),
+      scopes: [OAuth2Scopes.Bot, OAuth2Scopes.ApplicationsCommands],
+    });
+    const button = new ButtonBuilder()
+      .setLabel("Invite Link")
+      .setStyle(ButtonStyle.Link)
+      .setURL(link)
+      .setEmoji(resolvePartialEmoji("✉️"));
 
-		const AR = new ActionRowBuilder().addComponents(button);
+    const AR = new ActionRowBuilder().addComponents(button);
 
-		interaction.reply({
-			content: "Invite me by clicking the button.",
-			ephemeral: false,
-			components: [AR],
-		});
-	},
+    interaction.reply({
+      content: "Invite me by clicking the button.",
+      ephemeral: false,
+      components: [AR],
+    });
+  },
 };
