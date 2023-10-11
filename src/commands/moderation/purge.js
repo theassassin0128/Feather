d82b3b0963b2e1fd78564d2107a0a51fff4109b9
theassassin0128 <rafisarkar46@gmail.com>
2023-10-11 18:28:54 +0600

    purge.js === clear.js

---
+++ src/commands/moderation/purge.js
@@ -0,0 +1,94 @@
+const {
+  EmbedBuilder,
+  ChatInputCommandInteraction,
+  Client,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+} = require("discord.js");
+const { colours } = require("../../config.json");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("purge")
+    .setDescription("🗑️ Purge messages of a channel.")
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addIntegerOption((option) =>
+      option
+        .setName("amount")
+        .setDescription("The number of messages to delete.")
+        .setRequired(true)
+        .setMaxValue(100)
+        .setMinValue(1)
+    )
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("Select a Guild Member.")
+        .setRequired(false)
+    ),
+  test: true,
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
+   */
+  execute: async (interaction, client) => {
+    interaction.reply({
+      content: "Working on it .......",
+      ephemeral: true,
+    });
+    const amount = interaction.options.getInteger("amount");
+    const target = interaction.options.getUser("target");
+    const embed = new EmbedBuilder()
+      .setAuthor({
+        name: interaction.user.username,
+        iconURL: interaction.user.displayAvatarURL(),
+      })
+      .setTitle("TASK COMPLETED")
+      .setColor(colours.main)
+      .setFooter({
+        text: client.user.tag,
+        iconURL: client.user.displayAvatarURL(),
+      })
+      .setTimestamp();
+
+    if (target) {
+      var fetchedMessages = await interaction.channel.messages.fetch();
+      var messagesToDelete = [];
+      var i = 0;
+
+      fetchedMessages.filter((message) => {
+        if (message.author.id === target.id && amount > i) {
+          messagesToDelete.push(message);
+          i++;
+        }
+      });
+
+      var deletedMessages = await interaction.channel.bulkDelete(
+        messagesToDelete,
+        true
+      );
+
+      interaction.followUp({
+        embeds: [
+          embed.setDescription(
+            `🧹️ Purged \`${deletedMessages.size}\` messages of <@${target.id}>`
+          ),
+        ],
+        ephemeral: true,
+      });
+    } else {
+      var deletedMessages = await interaction.channel.bulkDelete(amount, true);
+
+      interaction.followUp({
+        embeds: [
+          embed.setDescription(
+            `🧹️ Purged \`${deletedMessages.size}\` messages from <#${interaction.channel.id}>`
+          ),
+        ],
+        ephemeral: true,
+      });
+    }
+  },
+};
